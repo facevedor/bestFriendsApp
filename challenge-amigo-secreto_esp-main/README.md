609bc1aaea3ec828c66a5bfa2216f1658c0977b4
fabio <fabioacevedo.r@gmail.com>
2025-07-29 13:53:14 -0400

    Create functions in app.js

---
+++ challenge-amigo-secreto_esp-main/README.md
@@ -0,0 +1,22 @@
+# 🎁 Best Friends App – Sorteo de Amigo Secreto
+
+Una aplicación web sencilla para organizar sorteos de amigo secreto entre amigos. ¡Ideal para fiestas, celebraciones o simplemente divertirse!
+
+## 🚀 Características
+
+- Agrega nombres de participantes.
+- Muestra la lista de amigos.
+- Realiza un sorteo aleatorio.
+- Muestra el resultado en pantalla.
+
+## 📚 Tecnologías Utilizadas
+
+- HTML
+- CSS
+- JavaScript
+
+## 🛠️ Cómo usar
+
+1. Clona el repositorio:
+   ```bash
+   git clone https://github.com/facevedor/bestFriendsApp.git
--- challenge-amigo-secreto_esp-main/app.js
+++ challenge-amigo-secreto_esp-main/app.js
@@ -1,1 +1,28 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let nombres = [];
+
+function agregarAmigo() {
+    let amigo = document.getElementById("amigo").value;
+    nombres.push(amigo);
+    document.getElementById("amigo").value = "";
+    console.log(nombres);
+    mostrarNombres();
+}
+function mostrarNombres() {
+    let ul = document.getElementById("listaAmigos");
+    ul.innerHTML = "";
+    for (let i = 0; i < nombres.length; i++) {
+        let li = document.createElement("li");
+        li.textContent = nombres[i];
+        ul.appendChild(li);
+    }
+}
+
+function sortearAmigo() {
+    let amigoSecreto = nombres[Math.floor(Math.random() * nombres.length)];
+    let ul = document.getElementById("resultado");
+    ul.innerHTML = "";
+    let li = document.createElement("li");
+    li.textContent = "El amigo secreto es: " + amigoSecreto;
+    ul.appendChild(li);
+}
